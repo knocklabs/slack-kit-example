c71ba3661e828b514bdd42f1b8de758c4584b10f
Jeff Everhart <jeffeverhart383@gmail.com>
2024-03-15 08:59:47 -0400

    update formatting

--- app/components/slack-auth-wrapper.tsx
+++ app/components/slack-auth-wrapper.tsx
@@ -5,20 +5,20 @@ import { SlackAuthButton, SlackAuthContainer } from "@knocklabs/react";
 
 export default function AuthWrapper() {
   const onAuthComplete = (result: string) => {
-    console.log("Result from Slack/Knock authentication:", result)
-	};
+    console.log("Result from Slack/Knock authentication:", result);
+  };
 
-	return (
-		<div>
-			<SlackAuthContainer
-				actionButton={
-					<SlackAuthButton
-						slackClientId={process.env.NEXT_PUBLIC_SLACK_CLIENT_ID!}
-						redirectUrl={process.env.NEXT_PUBLIC_REDIRECT_URL}
-						onAuthenticationComplete={onAuthComplete}
-					/>
-				}
-			/>
-		</div>
-	);
+  return (
+    <div>
+      <SlackAuthContainer
+        actionButton={
+          <SlackAuthButton
+            slackClientId={process.env.NEXT_PUBLIC_SLACK_CLIENT_ID!}
+            redirectUrl={process.env.NEXT_PUBLIC_REDIRECT_URL}
+            onAuthenticationComplete={onAuthComplete}
+          />
+        }
+      />
+    </div>
+  );
 }
